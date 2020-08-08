2c9bbb0eea96f9b121ed4c70f116b408cdda9536
Xiaobo2020 <1971822513@qq.com>
2020-08-08 23:20:50 +0800

    feat: 2. 两数相加

--- src/2/index.ts
+++ src/2/index.ts
@@ -19,10 +19,23 @@ class ListNode {
   }
 }
 
+// 方法一：时间复杂度O(max(m, n))，空间复杂度O(m + n)
+// 循环条件：l1存在 || l2存在 || 进位存在
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-  // 设置进位
   let cover = 0;
-  const ret = l1;
-  // TODO
-  return ret;
+  let ret = new ListNode(0, null);
+  let tmp = ret;
+  while(l1 !== null || l2 !== null || cover !== 0) {
+    const x = l1 ? l1.val : 0;
+    const y = l2 ? l2.val : 0;
+    const z = x + y + cover;
+    // 创建新节点
+    cover = Math.floor(z / 10);
+    const newNode = new ListNode(z % 10, null);
+    tmp.next = newNode;
+    tmp = newNode;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+  }
+  return ret.next;
 };
